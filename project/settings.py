8b9051d8ac78faf5c4350ca4773f315024517f55
Michel Rodrigues <michel.rodrigues86@yahoo.com.br>
2018-10-30 00:42:17 -0300

    testes gerando migrations a partir das models

--- project/settings.py
+++ project/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import sys
+from .utils import DisableMigrations
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -98,15 +100,9 @@ DATABASES = {
     }
 }
 
-MIGRATION_MODULES = {
-    'admin': None,
-    'auth': None,
-    'contenttypes': None,
-    'sessions': None,
-
-    'meetingroom': None,
-    'schedule': None,
-}
+if 'test' in sys.argv:
+    TESTS_IN_PROGRESS = True
+    MIGRATION_MODULES = DisableMigrations()
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
